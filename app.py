96a5ffaf2b0d8119760e38eecdfe590d4a9bef8d
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-27 17:43:36 +0530

    Single city plot done

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import streamlit as st
 
 final = pd.read_csv('Dataset/AQI and Lat Long of Countries.csv')
 final.dropna(inplace=True)
-l = list(sorted(final['City'].unique()))
-l.insert(0,'-')
+
 
 
 
@@ -17,16 +16,33 @@ def plot_by_country(dataframe, country, param1, param2):
 
     st.plotly_chart(fig, use_container_width=True)
 
+    st.subheader('Line chart of {} vs {} for top cities in {}'.format(param1, param2, country))
+
+
+def plot_by_country_city(dataframe, country,city, param1, param2):
+    temp_df = dataframe[(dataframe['Country'] == country) & (dataframe['City'] == city)]
+    fig = px.scatter_mapbox(temp_df, lat='lat', lon='lng', size=param1, color=param2, size_max=20, mapbox_style='carto-positron',
+    hover_name=temp_df['City'], height=900, width=1200, zoom=4)
+    
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+
 st.set_page_config(page_title='AQI Index', layout='wide')
 st.sidebar.header('AQI Globe')
 option = st.sidebar.radio('Choose', ['Check AQI', 'About the project'])
 if option == 'Check AQI':
     country = st.sidebar.selectbox('Select Country', list(sorted(final['Country'].unique())))
+
     temp_df = final[final['Country'] == country]
+    l = list(sorted(temp_df['City'].unique()))
+    l.insert(0,'-')
     city = st.sidebar.selectbox('Select a city in {}'.format(country), l)
+
     primary = st.sidebar.selectbox('Choose primary parameter', final.columns[2:12:2])
     secondary = st.sidebar.selectbox('Choose secondary parameter' ,[cols for cols in final.columns[2:12:2]if cols != primary])
     if city == '-':
         plot_by_country(final, country, primary, secondary)
     else:
-        st.write('pass')
+        plot_by_country_city(final, country,city,primary, secondary)
