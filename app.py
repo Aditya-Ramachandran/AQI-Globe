84a851a47f4fc3a1128d26be7badb6cf38a5d752
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-27 17:23:10 +0530

    Added a way thru which user can select only country if they want to

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import streamlit as st
 
 final = pd.read_csv('Dataset/AQI and Lat Long of Countries.csv')
 final.dropna(inplace=True)
+l = list(sorted(final['City'].unique()))
+l.insert(0,'-')
+
 
 
 def plot_by_country(dataframe, country, param1, param2):
@@ -19,8 +22,11 @@ st.sidebar.header('AQI Globe')
 option = st.sidebar.radio('Choose', ['Check AQI', 'About the project'])
 if option == 'Check AQI':
     country = st.sidebar.selectbox('Select Country', list(sorted(final['Country'].unique())))
-    # temp_df = final[final['Country'] == country]
-    # city = st.sidebar.selectbox('Select a city in {}'.format(country), list(sorted(temp_df['City'].unique())))
+    temp_df = final[final['Country'] == country]
+    city = st.sidebar.selectbox('Select a city in {}'.format(country), l)
     primary = st.sidebar.selectbox('Choose primary parameter', final.columns[2:12:2])
     secondary = st.sidebar.selectbox('Choose secondary parameter' ,[cols for cols in final.columns[2:12:2]if cols != primary])
-    plot_by_country(final, country, primary, secondary)
+    if city == '-':
+        plot_by_country(final, country, primary, secondary)
+    else:
+        st.write('pass')
