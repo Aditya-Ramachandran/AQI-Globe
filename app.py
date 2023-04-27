979d2243bcde00ed47161c1d4a3270812ac36e3a
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-27 16:50:52 +0530

    States dropdown added; now in alphabetical order

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import plotly.express as px
 import streamlit as st
 
 final = pd.read_csv('Dataset/AQI and Lat Long of Countries.csv')
-
+final.dropna(inplace=True)
 
 st.set_page_config(page_title='AQI Index', layout='wide')
 st.sidebar.header('AQI Globe')
 option = st.sidebar.radio('Choose', ['Check AQI', 'About the project'])
 if option == 'Check AQI':
-    st.sidebar.selectbox('Select Country', list(final['Country'].unique()))
+    country = st.sidebar.selectbox('Select Country', list(sorted(final['Country'].unique())))
+    temp_df = final[final['Country'] == country]
+    city = st.sidebar.selectbox('Select a city in {}'.format(country), list(sorted(temp_df['City'].unique())))
