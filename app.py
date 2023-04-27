5c8842d04fa318a38c7ccae14186862598c60991
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-27 16:41:07 +0530

    Country dropdown added

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import numpy as np
 import plotly.express as px
 import streamlit as st
 
+final = pd.read_csv('Dataset/AQI and Lat Long of Countries.csv')
+
+
 st.set_page_config(page_title='AQI Index', layout='wide')
 st.sidebar.header('AQI Globe')
 option = st.sidebar.radio('Choose', ['Check AQI', 'About the project'])
+if option == 'Check AQI':
+    st.sidebar.selectbox('Select Country', list(final['Country'].unique()))
