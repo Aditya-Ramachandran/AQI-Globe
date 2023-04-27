62dea400ed5b2173183694ccaa5c0e80042e5b08
Aditya Ramachandran <aditya.199303083@muj.manipal.edu>
2023-04-27 20:11:28 +0530

    Added the about page

--- app.py
+++ app.py
@@ -122,8 +122,8 @@ st.markdown("---")
 
 
 
-option = st.sidebar.radio('Choose', ['Check AQI', 'About the project'])
-if option == 'Check AQI':
+option = st.sidebar.radio('Choose', ['AQI Explorer', 'Learn about AQI Globe'])
+if option == 'AQI Explorer':
     country = st.sidebar.selectbox('Select Country', list(sorted(final['Country'].unique())))
 
     temp_df = final[final['Country'] == country]
@@ -161,3 +161,21 @@ if option == 'Check AQI':
             plot_by_country2(final, country, primary, secondary)
         else:
             plot_by_country_city2(final, country,city,primary, secondary)
+
+
+if option == 'Learn about AQI Globe':
+    st.subheader('About')
+    st.write('AQI Globe is an interactive web application that allows users to explore the air quality index (AQI) values of different cities worldwide. The AQI is a measure of how polluted the air is, and it takes into account several pollutants, such as carbon monoxide, ozone, and particulate matter. The higher the AQI value, the more polluted the air is and the more harmful it can be to human health.')
+    st.write('The app uses data from two datasets obtained from Kaggle, which were merged to provide a comprehensive view of AQI values across various countries and cities. The data is presented using Plotly, a Python library for creating interactive data visualizations.')
+    st.write('Users can select a country from a dropdown menu and choose between two options: viewing the overall AQI value for the country or selecting a specific city within the country. They can then select primary and secondary parameters from the dropdown menus to customize the graph. The primary parameter is represented by the size of the bubble, while the secondary parameter is represented by the color of the bubble.')
+    st.write('The application provides a simple and intuitive way for users to explore AQI values and gain insights into air pollution levels worldwide.')
+    st.markdown('---')
+
+    st.subheader('Explanation of common AQI terms')
+    st.write('* PM2.5: PM2.5 refers to tiny particles or droplets in the air that are 2.5 micrometers or less in width. They can be harmful to human health when inhaled, especially in high concentrations.')
+    st.write('* PM10: PM10 refers to particles or droplets in the air that are 10 micrometers or less in width. They can also be harmful to human health when inhaled, but generally to a lesser extent than PM2.5.')
+    st.write('* Ozone: Ozone is a gas that can form in the atmosphere through a chemical reaction between sunlight and other pollutants. High levels of ozone can be harmful to human health, particularly for those with respiratory issues.')
+    st.write('* Carbon Monoxide (CO): CO is a colorless, odorless gas that is produced by the incomplete burning of fossil fuels. High levels of CO can be toxic to humans and can cause headaches, dizziness, and nausea')
+    st.markdown('---')
+    st.subheader('Range of the particulate matter')
+    
